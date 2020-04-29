4ee16b97b918e6061eb604eae48d5b51ee293581
Shyam Reyal <shyam3001@gmail.com>
2020-04-29 19:16:58 +0530

    cleanup

--- tree-check.py
+++ tree-check.py
@@ -1,53 +1,44 @@
-print '##############Tree Checker##############'
+#!/usr/bin/env python
 import json
 import os
 
+# load keywords to dictionary
 keywords={}
-
 with open('keywords.json') as j_file:
     keywords = json.load(j_file)
 
-#output the keyword when value is given
+# output the keyword when value is given
 def getKey(v):
     for key, value in keywords.items():
         if(v in value):
-            print (key)
+            print (key, v)
 
-
-
-#open the solution
+# open the solution
 def openSolution():
     cur_path = os.getcwd()
     solution = open(cur_path+"/solutions/a/main.c", "r")
     return solution.read()
 
-#replace operators,semicolons, etc..
+# replace operators,semicolons, etc..
 def cleanCode(s):
     r = [';','+','=','/','*','-','[',']','(',')','"',':','{','}','//','#','<','>','\n',]
     for i in r:
         s = s.replace(i,' ')
 
     s = s.replace("'","")
-
-
     return s
 
-#slit the code into words
-
+# split the code into words
 def splitCode(s):
     w = s.split(" ")
-
     return w
 
-#Print tree
-
+# print tree
 def printTree(s):
     for i in s:
         getKey(i)
 
-
+# main
 fileText = openSolution()
-
 newFile = cleanCode(fileText)
-
 printTree(splitCode(newFile))
